b458d06f1ebd503743148e05b90f415db382c675
Andres Felipe Ospina GUalteros <andresfelipe.ospina@utp.edu.co>
2021-01-27 13:29:15 -0500

    cliente con sockets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
 
 function App() {
+  const [response, setResponse] = useState("");
+  const [socket, setSocket] = useState()
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT+'/admin');
+    setSocket(socket)
+    socket.on("Count", data => {
+      setResponse(data);
+    });
+    console.log(socket)
+    socket.emit("Join_office", "2")
+
+    return () => socket.disconnect();
+  }, []);
+
+  const Press = () =>{
+    socket.emit("Press")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <p>
+      Cantidad de veces presionado: {response}
+    </p>
+    <button onClick={()=>Press()}> Aqui </button>
     </div>
   );
 }
